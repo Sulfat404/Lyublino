da79d6892ba1feefc0a1b44ca05404abdf62b0ea
Liza Prokopenko <elizaveta_prokopenko@mail.ru>
2021-12-23 03:31:42 +0300

    start: adaptive after 992px

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
+// link become white
+const menu = document.querySelector('.menu');
+
+menu.addEventListener('mouseover', event => {
+    if (event.target.classList.contains('menu__item')) {
+        let link = event.target.children[0];
+        link.style.color = 'white';
+    }  
+});
+menu.addEventListener('mouseout', event => {
+    if (event.target.classList.contains('menu__item')) {
+        let link = event.target.children[0];
+        link.style.color = '#474747';
+    }
+});
+
+
+// carousel
 const carousel = document.querySelector('.carousel');
 const list = carousel.querySelector('ul');
 const listElem = carousel.querySelectorAll('li');
 const btnNext = carousel.querySelector('.next');
 const btnPrev = carousel.querySelector('.prev');
 const width = 583; 
-const count = 2.2;
+const count = 1;
 let position = 0;
 
 
@@ -18,7 +36,6 @@ function movePrev() {
 btnNext.addEventListener('click', moveNext); 
 
 function moveNext() {
-    console.log('click!');
     position -= width * count;
     position = Math.max(position, -width * (listElem.length - count));
     list.style.marginLeft = position + 'px';
